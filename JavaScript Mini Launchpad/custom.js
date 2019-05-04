9ca130f66fb64c299f92f6fcfcf7fc6d0ba790d7
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-05-04 17:02:18 +0800

    first commit

---
+++ JavaScript Mini Launchpad/custom.js
@@ -0,0 +1,16 @@
+  document.addEventListener('keydown', playSound);
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+  function playSound(e) {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0.5;
+    audio.play();
+  }
+  let keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
