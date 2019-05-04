faef23133962428acd1acb9662748190c29e6901
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-05-04 17:45:01 +0800

    adding comments for future reference

--- JavaScript Mini Launchpad/custom.js
+++ JavaScript Mini Launchpad/custom.js
@@ -1,16 +1,23 @@
+//when pressed the assigned keys, execute a function called playSound
   document.addEventListener('keydown', playSound);
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
   function playSound(e) {
+    //add attribute selector using ES6 template literal
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+    if (!audio) return; //stop the function from running
+
+    //perform the transition which stated on the class 'playing'.
     key.classList.add('playing');
-    audio.currentTime = 0.5;
+    audio.currentTime = 0.5; //rewind to the start (when pressed over & over again)
     audio.play();
   }
-  let keys = Array.from(document.querySelectorAll('.key'));
+  let keys = document.querySelectorAll('.key');
+  //loop over every single element & attach event listener
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  function removeTransition(e) {
+    //on the console ->TransitionEvent ->propertyName
+      if (e.propertyName !== 'transform') return; //skip it if it's not a transform
+      e.target.classList.remove('playing'); //remove the transition
+    }
