7df70cad480270f94ea1fc36cafd54c36ed32428
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-05-15 12:10:08 +0800

    added notes

--- CSS Variables and JavaScript/custom.js
+++ CSS Variables and JavaScript/custom.js
@@ -1,9 +1,17 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.controls input');  //.controls > input
 
 function handleUpdate() {
-const suffix = this.dataset.sizing || '';
+let suffix = this.dataset.sizing || '';
 document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+/* NOTES:
+
+dataset = object that contains all the data attributes from that specific
+element. In this project, sizing is the only data attribute (data-sizing).
+
+*/
