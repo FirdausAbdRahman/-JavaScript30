05c2c715266e34346b69931f7923bd6c4ff2546c
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-05-16 11:40:29 +0800

    need to work on responsiveness & reset function

--- CSS Variables and JavaScript/custom.js
+++ CSS Variables and JavaScript/custom.js
@@ -1,17 +1,29 @@
 const inputs = document.querySelectorAll('.controls input');  //.controls > input
+const reset = document.querySelector('.button');
+
 
 function handleUpdate() {
 let suffix = this.dataset.sizing || '';
 document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+// KIV
+function resetUpdate(){
+  let image = document.querySelector('.image');
+
+}
+
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+reset.addEventListener('click',resetUpdate);
+
 
 
 /* NOTES:
 
 dataset = object that contains all the data attributes from that specific
 element. In this project, sizing is the only data attribute (data-sizing).
+
+'' = for the color as it doesn't have a suffix.
 
 */
