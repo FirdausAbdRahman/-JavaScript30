7fa707ff5291a1d4ead5bb9f53079e8ccb00fca0
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-05-20 23:28:15 +0800

    adjust code a bit. Not significant anyway

--- CSS Variables and JavaScript Mini Photoshop/custom.js
+++ CSS Variables and JavaScript Mini Photoshop/custom.js
@@ -1,19 +1,18 @@
 const inputs = document.querySelectorAll('.controls input');  //.controls > input
 const reset = document.querySelector('.reset');
 
+
 function handleUpdate() {
 let suffix = this.dataset.sizing || '';
 document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-
 
 // Honestly, I'm not sure why this works !
 function resetUpdate(){
 return document.documentElement.style = "filter";
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-// inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach(input =>input.addEventListener('change', handleUpdate));
 reset.addEventListener('click',resetUpdate);
 
 
